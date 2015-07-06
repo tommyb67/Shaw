e27036de35499930d7851f16e7c6b4bc5f98f9bd
Tom Brennan <tommybrennan@gmail.com>
2015-07-06 11:05:35 -0400

    exercise 10 on shaw

---
+++ ex9.rb
@@ -0,0 +1,19 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# Listing the days of the week on a single line
+days = "Mon Tue Wen Thu Fri Sat Sun"
+
+# Showing the first 8 months of the year with new line characters
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print the days and months to the screen
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+# By using three quotes we can write over multiple lines
+puts """
+There's something going on here.
+With the three double quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we like, or 5 or 6.
+"""
