e2b98840c703e8ccfea03c44fb14cba9f5798cf4
Tom Brennan <tommybrennan@gmail.com>
2015-07-07 08:32:34 -0400

    some edit to ex13 and created ex14

--- ex13.rb
+++ ex13.rb
@@ -3,3 +3,7 @@ first, second, third = ARGV
 puts "Your first variable is #{first}"
 puts "Your second variable is #{second}"
 puts "Your third variable is #{third}"
+
+print "Do you like your choices? "
+answer = gets.chomp
+puts "Thank you for your answer of #{answer}."
